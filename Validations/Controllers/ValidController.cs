ea92f199bd877099eaa4c659c479817e4c7fd059
Rafal Pawlowski <rafal.pawlowski@outlook.com>
2019-03-19 17:34:05 +0100

    added validation

---
+++ Validations/Controllers/ValidController.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Validations.Models;
+
+namespace Validations.Controllers
+{
+    public class ValidController : Controller
+    {
+        [HttpGet]
+        public ActionResult Index()
+        {
+            AddUserVM model = new AddUserVM();
+            return View(model);
+        }
+
+        [HttpPost]
+        public ActionResult Index(AddUserVM model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+            return RedirectToAction("Index");
+        }
+
+        // GET: Valid/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Valid/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Valid/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Valid/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Valid/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Valid/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Valid/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ Validations/Models/AddUserVM.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Validations.Models
+{
+    public class AddUserVM
+    {
+        [Required(ErrorMessage = "Field first name is required")]
+        [StringLength(10, ErrorMessage = "Name must be max 10 chars")]
+        [Display(Name = "First name")]
+        public string FirstName { get; set; }
+
+        [Required(ErrorMessage = "Field last name is required")]
+        [StringLength(20, ErrorMessage = "Last name must be max 20 chars")]
+        [Display(Name = "Last name")]
+        public string LastName { get; set; }
+
+        [Required(ErrorMessage = "Field email address is required")]
+        [Display(Name = "Email address")]
+        [EmailAddress(ErrorMessage = "The Email Address is not valid")]
+        public string EmailAddress { get; set; }
+    }
+}
